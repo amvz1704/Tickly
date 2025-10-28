a207e2cfc6727d2022fb6aef75c4dc0440abf4e1
YuAnLee1 <yuanlee2004@gmail.com>
2025-10-28 18:05:40 -0500

    Ahora funciona en docker
    docker compose up --build
    en la consola para generar la imagen y correrlo, despues se queda guardado en el docker desktop. La base de datos corre de forma local en el pc, por lo que si se quiere probar sin docker en el archivo .env la linea DB_HOST=localhost
    debe decir localhost, si se quiere compilar la imagen y levantar en docker con el comando anterior se debe cambiar por DB_HOST=host.docker.internal

--- prueba/usuario-service/src/app.js
+++ prueba/usuario-service/src/app.js
@@ -13,3 +13,7 @@ app.use("/api", router);
 app.use(errorHandler);
 
 export default app;
+
+
+//OJO SI QUIERO CORRERLO LOCAL CAMBIAR host.docker.internal POR localhost
+//EN EL .evn FIJARSE!! para correr en docker debe ser host.docker.internal
